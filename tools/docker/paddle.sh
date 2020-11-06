8697d0869f37410bc1b8e7e3346130c1680a881e
root <root@bjyz-sys-gpu-kongming7.bjyz.baidu.com>
2020-11-06 09:25:23 +0000

    refine env script

--- tools/docker/paddle.sh
+++ tools/docker/paddle.sh
@@ -1,29 +1,14 @@
-#!/bin/bash
+#!/bin/bash -v
 
 # should specific this for loading model of @to_static
-mount_model_root_dir=$1
+original_model_root_dir=$1
+mount_dir_name="models"
+
+repo_dir=$(cd "$(dirname "$0")";cd ../..;pwd)
+mount_repo_name="inference_benchmark"
 
 # pull official image
-docker images pull hub.baidubce.com/paddlepaddle/paddle:latest-gpu-cuda10.1-cudnn7
+sudo docker images pull hub.baidubce.com/paddlepaddle/paddle:latest-gpu-cuda10.1-cudnn7
 
 # create container
-nvidia-docker run --name paddle_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace -it hub.baidubce.com/paddlepaddle/paddle:latest-gpu-cuda10.1-cudnn7  /bin/bash
-
-apt-get update & apt install libprotobuf-dev
-
-# attach container and prepare code
-cd /workspace
-
-# clone paddle and build infer lib
-git clone https://github.com/PaddlePaddle/Paddle.git
-cd Paddle
-mkdir build_infer and cd build_infer
-touch re_build.sh
-
-echo "#!/bin/bash" >> re_build.sh
-echo "cmake .. -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DWITH_MKL=ON -DWITH_GPU=ON -DON_INFER=ON -DWITH_PATHON=ON -DWITH_MKLDNN=OFF" >> re_build.sh
-echo "make -j$(nproc)" >> re_build.sh
-echo "make inference_lib_dist -j$(nproc)" >> re_build.sh
-
-# clone benchmark code
-git clone https://github.com/Aurelius84/Inference_benchmark.git
+sudo nvidia-docker run --name paddle_dy2stat_infer_cuda10 --net=host -v ${original_model_root_dir}:/workspace/$mount_dir_name -v ${repo_dir}:/workspace/$mount_repo_name -it hub.baidubce.com/paddlepaddle/paddle:latest-gpu-cuda10.1-cudnn7  /bin/bash
--- tools/docker/tensorflow.sh
+++ tools/docker/tensorflow.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# should specific this for loading model of @to_static
-mount_model_root_dir=$1
+original_model_root_dir=$1
+mount_dir_name="models"
+
+repo_dir=$(cd "$(dirname "$0")";cd ../..;pwd)
+mount_repo_name="inference_benchmark"
+
 
 # pull official images
-docker pull floopcz/tensorflow_cc:ubuntu-cuda
+sudo docker pull floopcz/tensorflow_cc:ubuntu-cuda
 
 # create container
-nvidia-docker run --name tf_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace -it floopcz/tensorflow_cc:ubuntu-cuda  /bin/bash
-
-# download repo
-git clone https://github.com/Aurelius84/Inference_benchmark.git
+sudo nvidia-docker run --name tf_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace/$mount_dir_name -v ${repo_dir}:/workspace/$mount_repo_name -it floopcz/tensorflow_cc:ubuntu-cuda  /bin/bash

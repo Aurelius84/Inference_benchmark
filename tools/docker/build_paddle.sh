1647322b7ac91b3e6528884922b24b1c96b661b6
root <root@bjyz-sys-gpu-kongming7.bjyz.baidu.com>
2020-11-06 09:25:23 +0000

    refine env script

---
+++ tools/docker/build_paddle.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -v
+
+paddle_dir=/workspace/Paddle
+build_dir=$paddle_dir/build_infer
+
+if [ ! -d "$paddle_dir" ];
+then
+    apt-get update & apt install libprotobuf-dev
+    wait
+
+    # attach container and prepare code
+    cd /workspace
+
+    # clone paddle and build infer lib
+    git clone https://github.com/PaddlePaddle/Paddle.git
+    wait
+
+    cd Paddle
+    mkdir build_infer and cd build_infer
+
+fi
+
+if [ ! -f "$build_dir/re_build.sh" ]
+then
+
+    cd $build_dir
+    touch re_build.sh
+
+    echo "#!/bin/bash" >> re_build.sh
+    echo "cmake .. -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DWITH_MKL=ON -DWITH_GPU=ON -DON_INFER=ON -DWITH_PATHON=ON -DWITH_MKLDNN=OFF" >> re_build.sh
+    echo "make -j$(nproc)" >> re_build.sh
+    echo "make inference_lib_dist -j$(nproc)" >> re_build.sh
+fi
+
+# apply latest code
+cd $build_dir
+git pull
+
+# re build paddle lib
+bash re_build.sh
--- tools/docker/torch.sh
+++ tools/docker/torch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# should specific this for loading model of @to_static
-mount_model_root_dir=$1
+original_model_root_dir=$1
+mount_dir_name="models"
+
+repo_dir=$(cd "$(dirname "$0")";cd ../..;pwd)
+mount_repo_name="inference_benchmark"
 
 # pull official images
-docker pull pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel
+sudo docker pull pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel
 
 # create container
-nvidia-docker run --name torch_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace -it pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel  /bin/bash
+sudo nvidia-docker run --name torch_dy2stat_infer_cuda10 --net=host -v ${original_model_root_dir}:/workspace/$mount_dir_name -v ${repo_dir}:/workspace/$mount_repo_name -it pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel  /bin/bash
 
 # update and download libtorch
 apt-get update & apt-get install wget git -y
 cd /workspace
 wget https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-1.6.0%2Bcu101.zip
 unzip libtorch-cxx11-abi-shared-with-deps-1.6.0+cu101.zip
-
-# download repo
-git clone https://github.com/Aurelius84/Inference_benchmark.git

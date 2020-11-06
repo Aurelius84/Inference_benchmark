31e73e822c80e98faf335393ee680a035437f281
Aurelius84 <zhangliujie@baidu.com>
2020-11-06 09:31:20 +0000

    Revert "refine env script"
    This reverts commit 8697d0869f37410bc1b8e7e3346130c1680a881e.

--- tools/docker/tensorflow.sh
+++ tools/docker/tensorflow.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-original_model_root_dir=$1
-mount_dir_name="models"
-
-repo_dir=$(cd "$(dirname "$0")";cd ../..;pwd)
-mount_repo_name="inference_benchmark"
-
+# should specific this for loading model of @to_static
+mount_model_root_dir=$1
 
 # pull official images
-sudo docker pull floopcz/tensorflow_cc:ubuntu-cuda
+docker pull floopcz/tensorflow_cc:ubuntu-cuda
 
 # create container
-sudo nvidia-docker run --name tf_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace/$mount_dir_name -v ${repo_dir}:/workspace/$mount_repo_name -it floopcz/tensorflow_cc:ubuntu-cuda  /bin/bash
+nvidia-docker run --name tf_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace -it floopcz/tensorflow_cc:ubuntu-cuda  /bin/bash
+
+# download repo
+git clone https://github.com/Aurelius84/Inference_benchmark.git
--- tools/docker/torch.sh
+++ tools/docker/torch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-original_model_root_dir=$1
-mount_dir_name="models"
-
-repo_dir=$(cd "$(dirname "$0")";cd ../..;pwd)
-mount_repo_name="inference_benchmark"
+# should specific this for loading model of @to_static
+mount_model_root_dir=$1
 
 # pull official images
-sudo docker pull pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel
+docker pull pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel
 
 # create container
-sudo nvidia-docker run --name torch_dy2stat_infer_cuda10 --net=host -v ${original_model_root_dir}:/workspace/$mount_dir_name -v ${repo_dir}:/workspace/$mount_repo_name -it pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel  /bin/bash
+nvidia-docker run --name torch_dy2stat_infer_cuda10 --net=host -v ${mount_model_root_dir}:/workspace -it pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel  /bin/bash
 
 # update and download libtorch
 apt-get update & apt-get install wget git -y
 cd /workspace
 wget https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-1.6.0%2Bcu101.zip
 unzip libtorch-cxx11-abi-shared-with-deps-1.6.0+cu101.zip
+
+# download repo
+git clone https://github.com/Aurelius84/Inference_benchmark.git
